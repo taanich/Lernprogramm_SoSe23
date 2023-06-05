0c3ea71cfaae3db98aa5ad3737b5dd4d0e627e7d
Tanja Dietrich <s83797@informatik.htw-dresden.de>
2023-06-05 23:46:07 +0200

    wip

--- mvp/mvp.js
+++ mvp/mvp.js
@@ -10,90 +10,73 @@ document.addEventListener('DOMContentLoaded', function () {
 // Model -------------------------------------------------------------------------------------------------------------
 class Model {
     constructor() {
-        // Array an Aufgaben
-        this.selectedTasks = new Set(); // Set, um bereits ausgewählte Aufgaben zu speichern
-        this.currentTask = null; // Aktuelle Aufgabe
-        this.totalQuestions = 0; // Gesamtanzahl der Fragen
-        this.taskCounts = {}; // Anzahl der Aufgaben pro Kategorie
+        this.selectedTasks = new Set();
+        this.currentTask = null;
+        this.totalQuestions = 0;
+        this.taskCounts = {};
+        this.data = null; // Variable zum Speichern der Aufgaben und Lösungen
     }
 
-    // Holt eine Frage aus dem Array, zufällig ausgewählt oder vom Server
-    getTask(category) {
+    async getTask(category) {
         console.log("Hole eine neue Aufgabe...");
-        return fetch("aufgaben.json")
-            .then((response) => response.json())
-            .then((data) => {
-                // Aufgaben und Lösungen anzeigen
-                this.showTask(data, category);
-                return this.currentTask; // Rückgabe der ausgewählten Aufgabe
-            })
-            .catch((error) => {
-                console.error("Fehler beim Laden der Aufgaben:", error);
-            });
-    }
-
-    // Methode, um die Anzahl der Aufgaben in der JSON-Datei für eine Kategorie zu erhalten
-    // Methode, um die Anzahl der Aufgaben in der JSON-Datei für eine Kategorie zu erhalten
-    async getTaskCount(category) {
-        if (this.taskCounts[category] !== undefined) {
-            // Falls die Anzahl der Aufgaben für die Kategorie bereits bekannt ist,
-            // geben wir den gespeicherten Wert zurück
-            return this.taskCounts[category];
-        } else {
-            // Ansonsten laden wir die JSON-Datei und ermitteln die Anzahl der Aufgaben
+        if (!this.data) {
             try {
-                const response = await fetch("aufgaben.json");
-                const data = await response.json();
-                const tasks = data[category];
-                const taskCount = tasks.length;
-                //this.taskCounts[category] = taskCount; // Speichern der Anzahl der Aufgaben für die Kategorie
-                this.totalQuestions = taskCount; // Setzen von totalQuestions auf die Anzahl der Aufgaben
-                console.log("Anzahl an geladenen Aufgaben: " + taskCount);
-                return taskCount;
+                this.data = await this.fetchTasks();
+                this.initializeTasks(category);
             } catch (error) {
                 console.error("Fehler beim Laden der Aufgaben:", error);
                 throw error;
             }
+        } else {
+            this.showRandomTask(category);
+        }
+        return this.currentTask;
+    }
+
+    async fetchTasks() {
+        try {
+            const response = await fetch("aufgaben.json");
+            return response.json();
+        } catch (error) {
+            console.error("Fehler beim Laden der Aufgaben:", error);
+            throw error;
         }
     }
 
-
-    resetTasks() {
-        this.selectedTasks.clear(); // Zurücksetzen der ausgewählten Aufgaben
+    initializeTasks(category) {
+        const tasks = this.data[category];
+        this.taskCounts[category] = tasks.length;
+        this.totalQuestions = tasks.length;
+        console.log("Anzahl an geladenen Aufgaben: " + tasks.length);
+        this.showRandomTask(category); // Aufgabe anzeigen
     }
 
-    showTask(data, category) {
-        const tasks = data[category];
+    resetTasks() {
+        this.selectedTasks.clear();
+        this.currentTask = null;
+    }
 
-        // Filtere die verbleibenden Aufgaben, um bereits ausgewählte Aufgaben auszuschließen
+    showRandomTask(category) {
+        const tasks = this.data[category];
         const remainingTasks = tasks.filter(task => !this.selectedTasks.has(task.a));
 
-        // Überprüfe, ob alle Aufgaben bereits ausgewählt wurden
         if (remainingTasks.length === 0) {
             console.log("Alle Aufgaben wurden bereits genommen.");
             this.currentTask = null;
             return;
         }
 
-        // Wähle eine zufällige Aufgabe aus den verbleibenden Aufgaben aus
         const randomTask = remainingTasks[Math.floor(Math.random() * remainingTasks.length)];
-
-        // Füge die ausgewählte Aufgabe dem Set der bereits ausgewählten Aufgaben hinzu
         this.selectedTasks.add(randomTask.a);
-
-        // Erhöhe die Gesamtanzahl der Fragen
-        this.totalQuestions = this.getTaskCount(category);
-
-        // Gib die ausgewählte Aufgabe zurück
         this.currentTask = randomTask;
         console.log("Aktuelle Aufgabe:", this.currentTask);
     }
-
 
     checkAnswer() {
         // TODO
     }
 }
+
 
 // Presenter ---------------------------------------------------------------------------------------------------------
 class Presenter {
@@ -117,8 +100,9 @@ class Presenter {
 
     resetTasks() {
         this.m.resetTasks();
-        this.m.totalQuestions = 0; // Gesamtanzahl zurücksetzen
-        this.correctCount = 0; // Zurücksetzen der korrekten Antworten
+        this.m.totalQuestions = 0;
+        this.correctCount = 0;
+        this.v.resetProgressBar(); // Neue Methode hinzugefügt
     }
 
     increaseCorrectCount() {
@@ -137,62 +121,56 @@ class View {
     }
 
     setHandler() {
-        let category = null;
+        const categoryMap = {
+            "math-start": "teil-mathe",
+            "internet-start": "teil-internettechnologien",
+            "allg-start": "teil-allgemein"
+        };
 
-        const mathStart = document.getElementById("math-start");
-        if (mathStart) {
-            mathStart.addEventListener("click", () => {
-                this.resetTasksAndStart("teil-mathe");
-                console.log("Neu Starten...");
-                category = "teil-mathe";
-            }, false);
-        }
+        const addStartEventHandler = (buttonId, category) => {
+            const startButton = document.getElementById(buttonId);
+            if (startButton) {
+                startButton.addEventListener("click", () => {
+                    this.resetTasksAndStart(category);
+                    console.log("Neu Starten...");
+                }, false);
+            }
+        };
 
-        const internetStart = document.getElementById("internet-start");
-        if (internetStart) {
-            internetStart.addEventListener("click", () => {
-                this.resetTasksAndStart("teil-internettechnologien");
-                console.log("Neu Starten...");
-                category = "teil-internettechnologien";
-            }, false);
+        for (const [buttonId, category] of Object.entries(categoryMap)) {
+            addStartEventHandler(buttonId, category);
         }
-
-        const allgStart = document.getElementById("allg-start");
-        if (allgStart) {
-            allgStart.addEventListener("click", () => {
-                this.resetTasksAndStart("teil-allgemein");
-                console.log("Neu Starten...");
-                category = "teil-allgemein";
-            }, false);
-        }
-
-        /*
-        const gradesStart = document.getElementById("grades-start");
-        if (gradesStart) {
-          gradesStart.addEventListener("click", () => this.start("teil-noten"), false);
-        }
-        */
 
         const weiterButton = document.getElementById("weiter-button");
-        weiterButton.addEventListener("click", () => {
-            this.p.m.getTask(category)
-                .then((frag) => {
-                    console.log("Erhaltene Aufgabe:", frag);
-                    this.renderText(frag, category, this.p.m.totalQuestions);
-                    // Weitere Logik oder Aktualisierung der Benutzeroberfläche bei Bedarf
-                })
-                .catch((error) => {
-                    console.error("Fehler beim Abrufen der Aufgabe:", error);
-                });
-        });
+        if (weiterButton) {
+            weiterButton.addEventListener("click", () => {
+                this.p.m.getTask(this.category)
+                    .then((frag) => {
+                        console.log("Erhaltene Aufgabe:", frag);
+                        this.renderText(frag, this.category, this.p.m.totalQuestions);
+                        // Weitere Logik oder Aktualisierung der Benutzeroberfläche bei Bedarf
+                    })
+                    .catch((error) => {
+                        console.error("Fehler beim Abrufen der Aufgabe:", error);
+                    });
+            });
+        }
     }
 
-    resetTasksAndStart(category){
+    resetTasksAndStart(category) {
         this.p.resetTasks();
         this.start(category);
+        this.updateStatsChart(this.p.correctCount); // Hier die Methode aufrufen, um die Anzeige zu aktualisieren
+    }
+
+    resetProgressBar() {
+        const progressBar = document.getElementById("progress-bar");
+        progressBar.style.backgroundColor = "green";
+        progressBar.style.width = "0%";
     }
 
     start(category) {
+        this.category = category;
         this.p.setTask(category);
     }
 
@@ -210,94 +188,99 @@ class View {
         taskElement.innerHTML = ""; // Löschen des vorherigen Inhalts
 
         if (!frag) {
-            const taskParagraph = document.createElement("p");
-            taskParagraph.innerHTML = "Alle Aufgaben wurden bereits genommen.";
-            taskElement.appendChild(taskParagraph);
-
-            const buttonsElement = document.getElementById("answers");
-            buttonsElement.innerHTML = ""; // Löschen der Antwort-Buttons
-
+            this.renderNoTasksMessage(taskElement);
+            this.clearAnswerButtons();
             return;
         }
 
-        if (category === "teil-mathe") {
-            // Aufgabe mit KaTeX rendern
-            const taskParagraph = document.createElement("p");
-            const taskExpression = katex.renderToString(frag.a, { throwOnError: false });
-            taskParagraph.innerHTML = "Ausgabe: " + taskExpression;
-            taskElement.appendChild(taskParagraph);
-        } else {
-            const taskParagraph = document.createElement("p");
-            taskParagraph.innerHTML = "Aufgabe: " + frag.a;
-            taskElement.appendChild(taskParagraph);
-        }
+        this.renderTask(frag, category, taskElement);
+        this.renderAnswerButtons(frag, category);
 
+        const progressBar = document.getElementById("progress-bar");
+        this.initializeProgressBar(progressBar);
+
+        this.addButtonHandlers(progressBar);
+    }
+
+    renderNoTasksMessage(taskElement) {
+        const taskParagraph = document.createElement("p");
+        taskParagraph.innerHTML = "Super! Du hast alle Aufgaben gelöst!";
+        taskElement.appendChild(taskParagraph);
+    }
+
+    clearAnswerButtons() {
+        const buttonsElement = document.getElementById("answers");
+        buttonsElement.innerHTML = "";
+    }
+
+    renderTask(frag, category, taskElement) {
+        const taskParagraph = document.createElement("p");
+        const taskText = (category === "teil-mathe") ? "Ausgabe: " : "Aufgabe: ";
+        taskParagraph.innerHTML = taskText + this.getTaskContent(frag, category);
+        taskElement.appendChild(taskParagraph);
+    }
+
+    getTaskContent(frag, category) {
+        return (category === "teil-mathe") ? katex.renderToString(frag.a, { throwOnError: false }) : frag.a;
+    }
+
+    renderAnswerButtons(frag, category) {
         const buttonsElement = document.getElementById("answers");
         buttonsElement.innerHTML = "";
 
         const answersWithIndices = frag.l.map((answer, index) => ({ answer, index }));
-        const correctAnswerIndex = answersWithIndices.findIndex(entry => entry.index === 0); // Index der richtigen Antwort speichern
+        const correctAnswerIndex = answersWithIndices.findIndex(entry => entry.index === 0);
 
-        if (category === "teil-mathe") {
-            //const answersWithIndices = frag.l.map((answer, index) => ({ answer, index }));
-            //const correctAnswerIndex = answersWithIndices.findIndex(entry => entry.index === 0); // Index der richtigen Antwort speichern
+        const shuffledAnswers = this.shuffleAnswers(answersWithIndices);
+        shuffledAnswers.forEach(({ answer, index }) => {
+            const button = document.createElement("button");
+            const buttonText = (category === "teil-mathe") ? katex.renderToString(answer, { throwOnError: false }) : answer;
 
-            const shuffledAnswers = this.shuffleAnswers(answersWithIndices);
-            shuffledAnswers.forEach(({ answer, index }) => {
-                const button = document.createElement("button");
-                const solutionExpression = katex.renderToString(answer, { throwOnError: false });
-                button.innerHTML = solutionExpression;
-                button.value = (index === correctAnswerIndex) ? "1" : "0"; // Wert 1 für richtige Antwort, Wert 0 für falsche Antworten
-                buttonsElement.appendChild(button);
-                adjustButtonWidth(button);
-            });
-        } else {
-            //const answersWithIndices = frag.l.map((answer, index) => ({ answer, index }));
-            //const correctAnswerIndex = answersWithIndices.findIndex(entry => entry.index === 0); // Index der richtigen Antwort speichern
+            button.innerHTML = buttonText;
+            button.value = (index === correctAnswerIndex) ? "1" : "0";
+            buttonsElement.appendChild(button);
+            this.adjustButtonWidth(button);
+        });
+    }
 
-            const shuffledAnswers = this.shuffleAnswers(answersWithIndices);
-            shuffledAnswers.forEach(({ answer, index }) => {
-                const button = document.createElement("button");
-                button.innerHTML = answer;
-                button.value = (index === correctAnswerIndex) ? "1" : "0"; // Wert 1 für richtige Antwort, Wert 0 für falsche Antworten
-                buttonsElement.appendChild(button);
-                adjustButtonWidth(button);
-            });
+    initializeProgressBar(progressBar) {
+        if (progressBar.style.width === "0%") {
+            progressBar.style.backgroundColor = "green";
+            progressBar.style.width = "0%";
         }
+    }
 
-        let incorrectCount = 0;
-
-        let progressBar = document.getElementById("progress-bar");
-        progressBar.style.backgroundColor = "green";
-        progressBar.style.width = "0%";
-
-        // Button-Handler hinzufügen
+    addButtonHandlers(progressBar) {
+        const buttonsElement = document.getElementById("answers");
         buttonsElement.querySelectorAll("button").forEach(button => {
             button.addEventListener("click", () => {
-                const answerValue = button.value;
-                if (answerValue === "1") {
-                    console.log("Richtige Antwort ausgewählt");
-                    this.p.increaseCorrectCount();
-                } else {
-                    console.log("Falsche Antwort ausgewählt");
-                    incorrectCount++;
-                }
-
-                // Nur die erste richtige Antwort zählt
-                if (!button.disabled) {
-                    button.disabled = true; // Deaktiviere den Button, um weitere Klicks zu verhindern
-                    this.updateStatsChart(this.p.correctCount); // Aktualisiere das Diagramm
-                }
-
+                this.handleAnswerSelection(button, progressBar);
+                this.updateStatsChart(this.p.correctCount); // Hier die Methode aufrufen, um die Anzeige zu aktualisieren
             });
         });
+    }
 
-        function adjustButtonWidth(button) {
-            const textWidth = button.scrollWidth;
-            const textHeight = button.scrollHeight;
-            button.style.width = textWidth + 'px';  // Anpassung der Breite
-            button.style.height = textHeight + 'px'; // Anpassung der Höhe
+    handleAnswerSelection(button, progressBar) {
+        const answerValue = button.value;
+        if (answerValue === "1") {
+            console.log("Richtige Antwort ausgewählt");
+            this.p.increaseCorrectCount();
+            this.updateProgressBarColor(progressBar, this.p.correctCount, this.p.m.totalQuestions);
+        } else {
+            console.log("Falsche Antwort ausgewählt");
         }
+
+        if (!button.disabled) {
+            button.disabled = true;
+            this.updateStatsChart(this.p.correctCount);
+        }
+    }
+
+    adjustButtonWidth(button) {
+        const textWidth = button.scrollWidth;
+        const textHeight = button.scrollHeight;
+        button.style.width = textWidth + 'px';
+        button.style.height = textHeight + 'px';
     }
 
     updateStatsChart(correctCount) {
@@ -305,5 +288,16 @@ class View {
         const totalQuestions = this.p.m.totalQuestions; // Gesamtanzahl der Fragen vom Model erhalten
         const correctPercentage = (correctCount / totalQuestions) * 100;
         progressBar.style.width = correctPercentage + "%";
+    }
+
+    updateProgressBarColor(progressBar, correctCount, totalQuestions) {
+        const correctPercentage = (correctCount / totalQuestions) * 100;
+        if (correctPercentage === 100) {
+            progressBar.style.backgroundColor = "green";
+        } else if (correctPercentage >= 50) {
+            progressBar.style.backgroundColor = "orange";
+        } else {
+            progressBar.style.backgroundColor = "red";
+        }
     }
 }
